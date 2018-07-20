415db84eeab187e48c2657eaca2bbe1933c603f4
Branislav Petrović <bpkinez@gmail.com>
2018-07-20 09:08:06 +0200

    Repair DriveInfo and XElement demo projects

--- Exam 70-483 Sample Applications/4.1 DriveInfo/Program.cs
+++ Exam 70-483 Sample Applications/4.1 DriveInfo/Program.cs
@@ -1,9 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.IO;
+using System;
 
 namespace _4._1_DriveInfo
 {
@@ -13,12 +12,12 @@ namespace _4._1_DriveInfo
         {
             DriveInfo[] drivesInfo = DriveInfo.GetDrives();
 
-            foreach(DriveInfo driveInfo in drivesInfo)
+            foreach (DriveInfo driveInfo in drivesInfo)
             {
                 Console.WriteLine("Drive {0}", driveInfo.Name);
                 Console.WriteLine("File type: {0}", driveInfo.DriveType);
 
-                if(driveInfo.IsReady == true)
+                if (driveInfo.IsReady == true)
                 {
                     Console.WriteLine("Volume label: {0}", driveInfo.VolumeLabel);
                     Console.WriteLine("File system: {0}", driveInfo.DriveFormat);
--- Exam 70-483 Sample Applications/4.3 XElement/Properties/AssemblyInfo.cs
+++ Exam 70-483 Sample Applications/4.3 XElement/Properties/AssemblyInfo.cs
@@ -2,7 +2,7 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("4.3 XElement")]
@@ -10,26 +10,26 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("4.3 XElement")]
-[assembly: AssemblyCopyright("Copyright ©  2014")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("9e14f45b-9746-4104-8a7a-f43ac461baf8")]
+[assembly: Guid("429767dc-8cf8-48d2-b294-a61e2ddb80ce")]
 
 // Version information for an assembly consists of the following four values:
 //
 //      Major Version
-//      Minor Version 
+//      Minor Version
 //      Build Number
 //      Revision
 //
-// You can specify all the values or you can default the Build and Revision Numbers 
+// You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
